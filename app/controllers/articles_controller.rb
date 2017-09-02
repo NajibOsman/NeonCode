1fbce8f58638dbcd2fbce0d6ff0dbb1a330c92b3
NajibOsman <machidydemajka38@gmail.com>
2017-09-02 14:49:27 +0300

    Initiall commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,81 @@
+class ArticlesController < ApplicationController
+  # GET /articles
+  # GET /articles.json
+  def index
+    @articles = Article.all
+  end
+
+  # GET /articles
+  # GET /articles.json
+  def show
+    @article = Article.find(params[:id])
+
+  end
+
+  # GET /articles/new
+  def new
+    @article  = current_user.articles.build
+  end
+
+  # GET /articles/edit
+  def edit
+    @article = Article.find(params[:id])
+
+  end
+
+  # Article /articles
+
+  # Article /articles.json
+  def create
+    @article = current_user.articles.build(article_params)
+
+    respond_to do |format|
+      if @article.save
+        format.html { redirect_to @article, notice: 'Article was successfully created.' }
+        format.json { render :show, status: :created, location: @article }
+      else
+        format.html { render :new }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /articles
+
+  # PATCH/PUT /articles.json
+  def update
+
+    respond_to do |format|
+      if @article.update(article_params[:id])
+        format.html { redirect_to @articles, notice: 'Article was successfully updated.' }
+        format.json { render :show, status: :ok, location: @article }
+      else
+        format.html { render :edit }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /articles
+  # DELETE /articles.json
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    respond_to do |format|
+      format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def article_params
+    params.require(:article).permit(:title, :content, :picture)
+  end
+
+end
